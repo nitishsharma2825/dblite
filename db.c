002bf9f0c9fa149155b39a6210a8eb2a18dbf30e
Nitish Sharma <sharnit2825@gmail.com>
2024-12-26 09:10:42 +0000

    p9:binary search for sorted keys and duplicate key

--- db.c
+++ db.c
@@ -55,7 +55,8 @@ typedef enum
 typedef enum
 {
     EXECUTE_SUCCESS,
-    EXECUTE_TABLE_FULL
+    EXECUTE_TABLE_FULL,
+    EXECUTE_DUPLICATE_KEY
 } ExecuteResult;
 
 typedef enum
@@ -132,6 +133,18 @@ typedef struct
     bool end_of_table; // Indicates a position one past the last element
 } Cursor;
 
+NodeType get_node_type(void* node)
+{
+    uint32_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+    return (NodeType)value;
+}
+
+void set_node_type(void* node, NodeType type)
+{
+    uint8_t value = type;
+    *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+}
+
 uint32_t* leaf_node_num_cells(void* node)
 {
     return node + LEAF_NODE_NUM_CELLS_OFFSET;
@@ -154,6 +167,7 @@ void* leaf_node_value(void* node, uint32_t cell_num)
 
 void initialize_leaf_node(void* node)
 {
+    set_node_type(node, NODE_LEAF);
     *leaf_node_num_cells(node) = 0;
 }
 
@@ -286,18 +300,55 @@ Cursor* table_start(Table* table)
     cursor->end_of_table = (num_cells == 0);
 }
 
-Cursor* table_end(Table* table)
+Cursor* leaf_node_find(Table* table, uint32_t page_num, uint32_t key)
 {
+    void* node = get_page(table->pager, page_num);
+    uint32_t num_cells = *leaf_node_num_cells(node);
+
     Cursor* cursor = malloc(sizeof(Cursor));
     cursor->table = table;
-    cursor->page_num = table->root_page_num;
+    cursor->page_num = page_num;
 
-    void* root_node = get_page(table->pager, table->root_page_num);
-    uint32_t num_cells = *leaf_node_num_cells(root_node);
-    cursor->cell_num = num_cells;
-    cursor->end_of_table = true;
-
+    // Binary Search
+    uint32_t min_index = 0;
+    uint32_t one_past_max_index = num_cells;
+    while (one_past_max_index != min_index)
+    {
+        uint32_t index = (min_index + one_past_max_index) / 2;
+        uint32_t key_at_index = *leaf_node_key(node, index);
+        if (key == key_at_index)
+        {
+            cursor->cell_num = index;
+            return cursor;
+        }
+        if (key < key_at_index)
+        {
+            one_past_max_index = index;
+        } else {
+            min_index = index + 1;   
+        }
+    }
+    
+    cursor->cell_num = min_index;
     return cursor;
+}
+
+/*
+Return the position of the given key.
+If the key is not present, return the position where it should be inserted
+*/
+Cursor* table_find(Table* table, uint32_t key)
+{
+    uint32_t root_page_num = table->root_page_num;
+    void* root_node = get_page(table->pager, root_page_num);
+
+    if (get_node_type(root_node) == NODE_LEAF)
+    {
+        return leaf_node_find(table, root_page_num, key);
+    } else {
+        printf("Need to implement searching an internal node\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 void* cursor_value(Cursor* cursor)
@@ -529,13 +580,24 @@ PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement)
 ExecuteResult execute_insert(Statement* statement, Table* table)
 {
     void* node = get_page(table->pager, table->root_page_num);
-    if ((*leaf_node_num_cells(node) >= LEAF_NODE_MAX_CELLS))
+    uint32_t num_cells = (*leaf_node_num_cells(node));
+    if (num_cells >= LEAF_NODE_MAX_CELLS)
     {
         return EXECUTE_TABLE_FULL;
     }
-
+    
     Row* row_to_insert = &(statement->row_to_insert);
-    Cursor* cursor = table_end(table);
+    uint32_t key_to_insert = row_to_insert->id;
+    Cursor* cursor = table_find(table, key_to_insert);
+
+    if (cursor->cell_num < num_cells)
+    {
+        uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
+        if (key_at_index == key_to_insert)
+        {
+            return EXECUTE_DUPLICATE_KEY;
+        }
+    }
 
     leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
 
@@ -622,6 +684,9 @@ int main(int argc, char* argv[])
         {
             case (EXECUTE_SUCCESS):
                 printf("Executed.\n");
+                break;
+            case (EXECUTE_DUPLICATE_KEY):
+                printf("Error: Duplicate key.\n");
                 break;
             case (EXECUTE_TABLE_FULL):
                 printf("Error: Table full.\n");
